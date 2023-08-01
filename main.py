f899c33816d67d1b220ec81f3f4f9af5bd0a195d
GonzaloSantizo <san21504@uvg.edu.gt>
2023-08-01 17:06:03 -0600

    changes to output file, final commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ rend.fragmentShader = shaders.fragmentShader
 rend.glLoadModel(filename="model.obj",textureName="model.bmp",translate=(400,height/2,0),rotate=(180,0,90),scale=(200,200,200))
 rend.glLoadModel(filename="model.obj",textureName="model.bmp",translate=(3*width/4,height/4,0),rotate=(180,0,180),scale=(200,200,200))
 rend.glLoadModel(filename="model.obj",textureName="model.bmp",translate=(1*width/4,height/4,0),rotate=(270,0,270),scale=(200,200,200))
-rend.glLoadModel(filename="model.obj",textureName="model.bmp",translate=(2*width/4,height/4,0),rotate=(90,0,270),scale=(200,200,200))
+rend.glLoadModel(filename="model.obj",textureName="model.bmp",translate=(3*width/4,height/2,0),rotate=(0,0,0),scale=(200,200,200))
 
 
 
